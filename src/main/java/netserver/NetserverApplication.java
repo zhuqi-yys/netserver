a8eaf21a870539a4df2e00d03178ec084f5e5d8b
zhuqi <1506411201@qq.com>
2019-12-12 19:22:30 +0800

    模型

---
+++ src/main/java/netserver/NetserverApplication.java
@@ -0,0 +1,13 @@
+package netserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/netserver/NetserverApplicationTests.java
@@ -0,0 +1,14 @@
+package netserver;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
